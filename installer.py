ab3042f896e1a10a92e1763b9a0abec2b93292b9
Xanii <73562093+LXanii@users.noreply.github.com>
2023-01-07 23:32:03 -0500

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("pip install pytest-shutil")
+os.system("pip install googlesearch-python")
+
+input("\nIf any errors, check this video: https://www.youtube.com/watch?v=cm6WDGAzDPM&ab_channel=PyTutorials")
